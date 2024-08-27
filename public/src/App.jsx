c549213b25014830b47ee8aa29d820208bb6809f
Gerald Simanullang <geraldsimanullang@gmail.com>
2024-08-27 13:10:20 +0700

    setup public

---
+++ public/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='min-h-screen bg-white'>
+    <div className='text-3xl font-semibold text-purple-500'>
+      Hello diks
+    </div>
+  </div>
+  )
+}
+
+export default App
