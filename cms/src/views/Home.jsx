e58ff80892fa1c4e5cd85b180b8dc43f58772395
Gerald Simanullang <geraldsimanullang@gmail.com>
2024-08-30 13:20:29 +0700

    feat: loading animation

--- cms/src/views/Home.jsx
+++ cms/src/views/Home.jsx
@@ -1,24 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Loading from "../assets/Loading.svg";
 
-export default function Home({serverUrl}) {
+export default function Home({ serverUrl }) {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   async function getProducts() {
     try {
-      const { data } = await axios.get(
-        `${serverUrl}/products`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-          },
-        }
-      );
+      setLoading(true);
+      const { data } = await axios.get(`${serverUrl}/products`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
 
       setProducts(data.data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -44,17 +46,19 @@ export default function Home({serverUrl}) {
     try {
       event.preventDefault();
 
-      await axios.delete(`https://server.geraldsimanullang.site/products/${id}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+      await axios.delete(
+        `https://server.geraldsimanullang.site/products/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
         }
-      })
+      );
 
       getProducts();
-      
     } catch (error) {
-      console.log(error)
-    } 
+      console.log(error);
+    }
   }
 
   useEffect(() => {
@@ -63,71 +67,81 @@ export default function Home({serverUrl}) {
 
   return (
     <>
-      <div className="overflow-x-auto p-10">
-        <table className="table text-black">
-          {/* head */}
-          <thead className="text-black">
-            <tr>
-              <th></th>
-              <th>Product Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Stock</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* rows */}
-            {products.map((el) => {
-              return (
-                <>
-                  <tr>
-                    <td>
-                      <div className="avatar">
-                        <div className="mask mask-squircle h-12 w-12">
-                          <img src={el.imgUrl} alt="" />
+      {loading ? 
+
+          <div className="min-h-screen flex flex-col items-center mt-32 ">
+            <img src={Loading} className="size-16"/>
+            <p>fetching data...</p>
+          </div>
+       : 
+        <div className="overflow-x-auto p-10">
+          <table className="table text-black">
+            {/* head */}
+            <thead className="text-black">
+              <tr>
+                <th></th>
+                <th>Product Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Stock</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* rows */}
+              {products.map((el) => {
+                return (
+                  <>
+                    <tr>
+                      <td>
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img src={el.imgUrl} alt="" />
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="flex items-center gap-3">
-                        <div>
-                          <div className="font-bold">{el.name}</div>
+                      </td>
+                      <td>
+                        <div className="flex items-center gap-3">
+                          <div>
+                            <div className="font-bold">{el.name}</div>
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="max-w-96">{el.description}</p>
-                    </td>
-                    <td>
-                      <p>{displayPrice(el.price)}</p>
-                    </td>
-                    <td>
-                      <p>{el.stock}</p>
-                    </td>
-                    <td>
-                      <div className="flex gap-2">
-                        <button
-                          className="btn btn-active btn-primary"
-                          onClick={(event) => handleEditButton(event, el.id)}
-                        >
-                          Edit
-                        </button>
-                        <button
-                          className="btn btn-error"
-                          onClick={(event) => handleDeleteButton(event, el.id)}
-                        >
-                          Delete
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                </>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+                      </td>
+                      <td>
+                        <p className="max-w-96">{el.description}</p>
+                      </td>
+                      <td>
+                        <p>{displayPrice(el.price)}</p>
+                      </td>
+                      <td>
+                        <p>{el.stock}</p>
+                      </td>
+                      <td>
+                        <div className="flex gap-2">
+                          <button
+                            className="btn btn-active btn-primary"
+                            onClick={(event) => handleEditButton(event, el.id)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="btn btn-error"
+                            onClick={(event) =>
+                              handleDeleteButton(event, el.id)
+                            }
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      }
     </>
   );
 }
